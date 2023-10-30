d6386a0000ad898a0119b526cff586dd30aff2d1
openusp <openuspio@gmail.com>
2023-10-30 13:48:06 +0530

    obuspa docker change to incorporate multistage

--- scripts/bash/aliases.sh
+++ scripts/bash/aliases.sh
@@ -1,3 +1,3 @@
 alias dc="docker compose -f ./deployments/docker-compose.yaml" 
 alias dclocal="docker compose -f ./deployments/docker-compose_local.yaml" 
-alias cli="docker run --env-file configs/openusp.env --network=openusp -it n4networks/openusp-cli"
+alias cli="docker run --env-file configs/openusp.env --network=openusp -it --rm n4networks/openusp-cli"
